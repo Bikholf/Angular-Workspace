ecd691041279d4b3cd430c6acd269e52c66dda13
Nick Horstkott <nick.horstkott@moguntia.com>
2025-01-08 15:03:34 +0100

    first commit

---
+++ projects/first-app/app/app.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import {HomeComponent} from './home/home.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [HomeComponent],
+  template: `
+    <h1>Hello world!</h1>
+  `,
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'homes';
+}
